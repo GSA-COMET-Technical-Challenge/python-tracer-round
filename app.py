6447fc63bdfb72fa94528e65f17dfea5dd626dc5
davidcallner <33468352+davidcallner@users.noreply.github.com>
2021-09-30 10:48:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import os
 import json
 from flask import request, jsonify
 from datetime import datetime
-from query import get_best_tle, get_ais, hits_and_misses, cheat, hits_and_misses_smarter, get_hits_and_misses, get_hits_and_misses_given_ais, get_hits_and_misses_smarter, get_hits_and_misses_mp, split, get_hits_and_misses_mp_alt, get_hits_and_misses_smarter_mp, get_sat_positions
-from convert import getLonLatHits, getLonLatMisses
-from create_czml import getCZML
-
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
